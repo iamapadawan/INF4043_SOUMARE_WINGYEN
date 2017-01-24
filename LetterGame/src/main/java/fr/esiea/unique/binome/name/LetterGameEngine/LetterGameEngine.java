45c3b73571841aac3cb0dc89bc533a22cf1bf1b2
Nguyen <Nguyen@msi-Nguyen>
2017-01-24 19:16:27 +0100

    player et lettergameengine

--- LetterGame/src/main/java/fr/esiea/unique/binome/name/LetterGameEngine/LetterGameEngine.java
+++ LetterGame/src/main/java/fr/esiea/unique/binome/name/LetterGameEngine/LetterGameEngine.java
@@ -1,5 +1,16 @@
 
 public class LetterGameEngine {
 
+	private SharedBag shareBag;
+	private Player player1;
+	private Player player2;
 	
+	public LetterGameEgine(){
+		
+	}
+	
+	
+	public static void main(args[]){
+		
+	}
 }
