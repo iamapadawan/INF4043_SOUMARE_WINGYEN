1bbbbdc0a13f7079e92210950e34b00dcb0f4795
Nguyen <Nguyen@msi-Nguyen>
2017-02-06 09:36:39 +0100

    test main

--- LetterGame/src/main/java/fr/esiea/unique/binome/name/LetterGameEngine/LetterGameEngine.java
+++ LetterGame/src/main/java/fr/esiea/unique/binome/name/LetterGameEngine/LetterGameEngine.java
@@ -6,11 +6,16 @@ public class LetterGameEngine {
 	private Player player2;
 	
 	public LetterGameEgine(){
+		File file = new File("./LetterGames/src/main/resources/dico.txt");
+		if(file.exists() == true) { 
+		System.out.println("true");
+		}
+		
 		
 	}
 	
 	
-	public static void main(args[]){
-		
+	public static void main (string[] args){
+		LetterGameEngine g= new LetterGameEngine();		
 	}
 }
