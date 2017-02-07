6d7e8fa50fc5f44a33e38a84ef76e3031a251ef1
ahmedsalem <ahmedsalem@Teemonito>
2017-02-07 13:32:27 +0100

    -SharedBag implementation

--- LetterGame/src/main/java/fr/esiea/unique/binome/name/LetterGameEngine/SharedBag.java
+++ LetterGame/src/main/java/fr/esiea/unique/binome/name/LetterGameEngine/SharedBag.java
@@ -1,18 +1,36 @@
 package fr.esiea.unique.binome.name.LetterGameEngine;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import com.sun.istack.internal.logging.Logger;
 
 public class SharedBag {
 
-    private ArrayList<Character> lettersList = new ArrayList<Character>();
+    private Logger mLogger = Logger.getLogger(SharedBag.class);
+    private ArrayList<Character> lListOfLetterOnSharedBag;
 
     public SharedBag() {
-        this.lettersList = new ArrayList<Character>();
+        this.lListOfLetterOnSharedBag = new ArrayList<Character>();
     }
 
-    public boolean addLetter(Character pletter) {
-
-        lettersList.add(pletter);
+    public boolean addLetter(List<Character> pListOfletter) {
+        if (pListOfletter == null || pListOfletter.isEmpty()) {
+            return false;
+        }
+        for (Character lLetterToAdd : pListOfletter) {
+            lListOfLetterOnSharedBag.add(lLetterToAdd);
+            // Show the letter added to the shared Bag
+            mLogger.info("letter added to the shared Bag: " + lLetterToAdd.toString());
+        }
         return true;
+    }
+
+    public boolean clearBag() {
+        lListOfLetterOnSharedBag.clear();
+        if (lListOfLetterOnSharedBag.isEmpty()) {
+            return true;
+        }
+        return false;
     }
 }
--- LetterGame/src/main/test/java/fr/esiea/unique/binome/name/dictionary/SharedBagTest.java
+++ LetterGame/src/main/test/java/fr/esiea/unique/binome/name/dictionary/SharedBagTest.java
@@ -5,23 +5,37 @@ import com.sun.xml.internal.ws.policy.AssertionSet;
 
 import fr.esiea.unique.binome.name.LetterGameEngine.SharedBag;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.assertj.core.api.Assertions;
 /*
  * Creation : 6 févr. 2017
  */
 
 public class SharedBagTest{
-    import SharedBag;
+    SharedBag lSharedBag = new SharedBag();
     
     @Before
     public void Setup(){
+        //TODO see if correct Ahmed
+        //check if the list of letter is initialised and empty
+        Assertions.assertThat(lSharedBag.clearBag()).isTrue();       
     }
     
     @Test
-    public void TestGetExistingLetterInTheBag(){
-        //We will try to get not existing letter from the bag
-           
-        
+    public void TestAddLetterInTheBag(){
+        //We will try to add letter in the bag
+        Assertions.assertThat(lSharedBag.addLetter(this.createListOfLetter()).isTrue();
+    }
+    private List <Character> createListOfLetter(){
+        List<Character> lListOfLetter = new ArrayList<>();
+        lListOfLetter.add("a");
+        lListOfLetter.add("h");
+        lListOfLetter.add("m");
+        lListOfLetter.add("e");
+        lListOfLetter.add("d");
+        return lListOfLetter;
     }
 
 }
